238df17c2ed38ff8d08e97d6e2d072dde4fe932e
AnirudhRKG <54069149+AnirudhRKG@users.noreply.github.com>
2022-03-27 02:10:46 +0530

    Weapon added

---
+++ Mini Knight/Assets/Scripts/AimCharacter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCharacter : MonoBehaviour
+{
+    private Transform AimTransform;
+    Vector2 direction;
+
+    public Transform barrel;
+    public GameObject bullet;
+    public float bulletSpeed = 50;
+    public string Bullets;
+    public int GunDurability = 3;
+    public GameObject Aimm;
+
+    private ObjectPooler objectpool;
+
+    private void Start()
+    {
+        objectpool = FindObjectOfType<ObjectPooler>();
+    }
+
+    private void Awake()
+    {
+        AimTransform = transform.Find("Aim");
+    }
+
+    void Update()
+    {
+        //Aim at Mouse Pointer
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        AimTransform.up = direction;
+
+        //Shooting
+        if(GunDurability >= 1)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                GameObject bulletc = objectpool.SpawnFromPool(Bullets , barrel.position , Quaternion.Euler(0, 0, 1));
+                bulletc.GetComponent<Rigidbody>().velocity = barrel.right * bulletSpeed;
+                GunDurability--;
+            }
+        }
+        if(GunDurability == 0)
+        {
+            Aimm.SetActive(false);
+        }
+    }
+}
